5ec0f9c8f35a88525d28f801f28997a7e2946866
BNHDeveloper <gussisabrin@gmail.com>
2025-12-21 12:30:09 +0100

    Add NavBar [Drawer] in the home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,17 +7,67 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: const Color(0xFFFFFFFF),
+      drawer: Drawer(
+        backgroundColor: const Color(0xFFF6F6F6),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.black12,
+                    backgroundImage: AssetImage('assets/img/avatar2.png'),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Nour Elhouda',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'easycodedz@example.com',
+                    style: TextStyle(color: Colors.black54),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+            leading: Icon(Icons.home,color: red,size: 22,),
+             title: Text('Home',style: TextStyle(fontWeight: FontWeight.bold),),onTap: null,),
+            ListTile(
+            leading: Icon(Icons.shopping_cart_rounded,color: red,size:22,),
+            title: Text('Cart',style: TextStyle(fontWeight: FontWeight.bold),),onTap: null,),
+            ListTile(
+            leading: Icon(Icons.person,color: red,size: 22,),
+            title: Text('Profile',style: TextStyle(fontWeight: FontWeight.bold),),onTap: null,),
+            ListTile(
+            leading: Icon(Icons.message,color: red,size: 22,),
+            title: Text('chat',style: TextStyle(fontWeight: FontWeight.bold),),onTap: null,),
+          ],
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        leading: IconButton(
-          onPressed: () {
-            // my code
-          },
-          icon: Container(
-            padding: const EdgeInsets.all(8),
-            child: const Icon(Icons.menu, color: Colors.black, size: 24),
+        leading: Builder(
+          builder: (context) => IconButton(
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            icon: Container(
+              padding: const EdgeInsets.all(8),
+              child: const Icon(Icons.menu, color: Colors.black, size: 24),
+            ),
           ),
         ),
         title: Row(
@@ -50,7 +100,7 @@ class Home extends StatelessWidget {
             child: CircleAvatar(
               radius: 20,
               backgroundColor: Colors.grey.shade200,
-              backgroundImage: const AssetImage('assets/img/avatar.png'),
+              backgroundImage: const AssetImage('assets/img/avatar2.png'),
               child: Container(
                 decoration: BoxDecoration(
                   shape: BoxShape.circle,
