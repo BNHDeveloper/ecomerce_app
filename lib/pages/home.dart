75032937304c1be89b49f31099a15dcd73177a11
BNHDeveloper <gussisabrin@gmail.com>
2025-12-22 11:23:24 +0100

    Add Special Offer in the Home Page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:project/Constatnt/constant.dart';
 import 'package:project/widget/NavBarSection.dart';
 import 'package:project/widget/NavHaeder.dart';
-import 'package:project/widget/inputFailed.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -13,22 +12,21 @@ class Home extends StatelessWidget {
       backgroundColor: const Color(0xFFFFFFFF),
       drawer: Drawer(
         backgroundColor: const Color(0xFFF6F6F6),
-        child:NavBarSection()
+        child: NavBarSection(),
       ),
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        title: 
-        NavHaeder()
+        title: NavHaeder(),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           children: [
             Container(
               margin: EdgeInsets.all(15),
               padding: EdgeInsets.all(10),
               alignment: Alignment.center,
-              width: 350,
+              width: 370,
               height: 50,
               decoration: BoxDecoration(
                 color: const Color.fromARGB(15, 251, 0, 0),
@@ -37,7 +35,7 @@ class Home extends StatelessWidget {
               child: TextField(
                 keyboardType: TextInputType.text,
                 obscureText: false,
-                decoration:InputDecoration(
+                decoration: InputDecoration(
                   border: InputBorder.none,
                   prefixIcon: Icon(Icons.search),
                   hintText: "Search",
@@ -45,11 +43,81 @@ class Home extends StatelessWidget {
                     color: grey,
                     fontWeight: FontWeight.w600,
                   ),
-                )
+                ),
               ),
-            )
+            ),
+            Container(
+              width: double.infinity,
+              margin: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  colors: [
+                    Color.fromARGB(255, 253, 39, 39),
+                    Color.fromARGB(255, 248, 90, 90),
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Stack(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Special Offer for March',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'Always to launch this tour',
+                          style: TextStyle(
+                            color: const Color(0xFFFFFFFF),
+                            fontSize: 14,
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            foregroundColor: red,
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 10,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                          child: Text(
+                            "Buy Now",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    right: -80,
+                    bottom: -20,
+                    child: Image.asset('assets/img/home.png', width: 280),
+                  ),
+                ],
+              ),
+            ),
           ],
-        )
+        ),
       ),
     );
   }
